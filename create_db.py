80adab00ece34b5cb2bf1dc49801428beb7f5ffe
prernaagarwal <prerna214@gmail.com>
2018-03-10 14:52:17 -0800

    data type of latitudes and longitudes updated

--- create_db.py
+++ create_db.py
@@ -12,14 +12,14 @@ try:
     cur = con.cursor()
     cur.execute('CREATE EXTENSION cube');
     cur.execute('CREATE EXTENSION earthdistance');
-    cur.execute("CREATE TABLE loc(key TEXT PRIMARY KEY NOT NULL, place_name VARCHAR(100) NOT NULL, admin_name1 VARCHAR(100) NOT NULL, latitude REAL, longitude REAL, accuracy INT)")
+    cur.execute("CREATE TABLE loc(key TEXT PRIMARY KEY NOT NULL, place_name VARCHAR(100) NOT NULL, admin_name1 VARCHAR(100) NOT NULL, latitude DOUBLE PRECISION, longitude DOUBLE PRECISION, accuracy INT)")
 
    # with open('/home/prerna/IN.csv', 'r') as file:
    #     reader = csv.reader(file)
    #     next(file)
    #     cur.copy_from(file, 'loc', sep=',', columns=('key','place_name','admin_name1','latitude','longitude','accuracy'))
     
-    cur.execute("COPY loc(key, place_name, admin_name1,latitude,longitude,accuracy) FROM '/home/prerna/IN.csv' DELIMITER ',' CSV HEADER")
+    cur.execute("COPY loc(key, place_name, admin_name1,latitude,longitude,accuracy) FROM '/home/prerna/locations/IN.csv' DELIMITER ',' CSV HEADER")
     con.commit()
 except psycopg2.DatabaseError, e:
     if con:
