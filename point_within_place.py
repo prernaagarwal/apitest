f31c5dfa792e2872648b14927e70f550016b1bcd
prernaagarwal <prerna214@gmail.com>
2018-03-11 13:26:27 -0700

    app works, need to take input from user in url

--- point_within_place.py
+++ point_within_place.py
@@ -3,10 +3,13 @@
 from flask import Flask, json, request
 from location1 import Location
 from point import Point1
+import psycopg2
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
-import psycopg2
 
+app = Flask(__name__)
+
+con = None
 
 try:
     con = psycopg2.connect("host='localhost' dbname='boundaries' user='postgres' password='password'")
@@ -15,12 +18,18 @@ try:
 except:
     print ("I am unable to connect to the database")
 
+@app.route('/')
+def index():
+    return "Hello, World!"
 
+
+
+@app.route('/containing_place', methods=['GET'])
 def get_place():
-        lat =  28.4229575890799
+        lat =  0
         lon =  77.4143028259277
         p = Point1(lat, lon, 0)
-        print(lat,lon)
+        print(lon,lat)
         return find_place(p)
 
 def find_place(p):
@@ -38,29 +47,28 @@ def find_place(p):
                 'place' : each_place
             }
             )
+
         new_rows = cursor.fetchall()
         coordinates = []
         for new_row in new_rows:
             coordinates.append((new_row[6], new_row[5]))
-        #https://stackoverflow.com/questions/43892459/check-if-geo-point-is-inside-or-outside-of-polygon-in-python
         
+        #https://stackoverflow.com/questions/43892459/check-if-geo-point-is-inside-or-outside-of-polygon-in-python
         polygon = Polygon(coordinates)
-
         point = Point(p.long1,p.lat1)
-        #print point
-        sample_point = (p.long1,p.lat1)
 
+        sample_point = (p.long1,p.lat1)
         for coord in coordinates:    #check if point is on the boundary
             if coord == sample_point:
-                print each_place
+                return json.dumps(each_place)
                 break  
         
         if(polygon.contains(point)):
-            print each_place  
-    return each_place
+            return json.dumps(each_place)  
+    return json.dumps("The location is not contained within given cities")
 
 if __name__ == '__main__':
     get_place()
-    #app.run(debug=True)
+    app.run(debug=True)
     cursor.close()
     con.close()
